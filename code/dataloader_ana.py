2703b9efcca4aed7edddacad96d12239ae349e99
Anna Silva Martins <amartins@stbc-i1.nikhef.nl>
2022-12-02 16:39:09 +0100

    first version of some kind of dataloader?

--- code/dataloader_ana.py
+++ code/dataloader_ana.py
@@ -1,11 +1,33 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.datasets import load_iris
+from torch.utils.data import Dataset
+import os
 
 path_to_data = "../../../lopezm/ML_projects/Projects_2022/Project_3/Data/dataframes/"
 
-blip_h1_o3a = pd.read_csv(path_to_data + "Blip_H1_O3a.csv")
+class Data(Dataset):
+    """
+    class that reads and initializes the data
+    """
 
-blip_h1_o3a = blip_h1_o3a.sort_values(by=["Event ID"])
-print(blip_h1_o3a)
+    def __init__(self, filename, path_to_data):
+        """
+        initialization of the object
+        """
+        self.data = pd.read_csv(path_to_data + filename)
+        self.filename = str(filename)
+    
+    def print(self):
+        """
+        printing the data
+        """
+        print(self.filename + str(self.data))
+        
+data = []
+i = 0
+
+filenames = os.listdir(path_to_data)
+
+for filename in filenames:
+    if ".csv" in filename:
+        data.append(Data(filename, path_to_data))
+    
