7b2898bf98f66bd6f99d336d7260b55816a15a65
ana <anaisabelmartins@tecnico.ulisboa.pt>
2022-12-08 12:13:23 +0100

    more

--- code/plots_ana.py
+++ code/plots_ana.py
@@ -3,5 +3,5 @@ def plots(blip, param1, param2, param1_name, param2_name):
     plt.scatter(injections.param1, train_data_injections[0].param2, label = "Injections", s = 1)
     plt.legend()
     plt.title(param1_name + " vs " + param2_name)
-    plt.savefig(param1_name + "vs" + param2_name + ".png")
+    plt.savefig("plots/" + param1_name + "vs" + param2_name + ".png")
     plt.clf()
