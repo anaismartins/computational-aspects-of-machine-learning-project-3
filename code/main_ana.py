3bbca0ae95e05d5d708da8f2165ab48a7c8f3ada
Anna Silva Martins <amartins@stbc-i1.nikhef.nl>
2022-12-08 21:20:31 +0100

    separated plotting

--- code/main_ana.py
+++ code/main_ana.py
@@ -1,6 +1,5 @@
 # general modules
 import os
-import matplotlib.pyplot as plt
 
 #torch
 from torch.utils.data import DataLoader
@@ -8,7 +7,6 @@ from torch.utils.data import DataLoader
 # my modules
 from LoadCSV_ana import LoadCSV
 import globals_ana as g
-from plots import plots
         
 train_data_blip = []
 test_data_blip = []
@@ -32,32 +30,3 @@ for filename in filenames:
     
 #train_dataloader = DataLoader(train_data, batch_size = g.batch_size, shuffle = True)
 #test_dataloader = DataLoader(test_data, batch_size = g.batch_size, shuffle = True)
-
-# plot
-
-plots(train_data_blip[0], train_data_injections[0], snr, snr, "SNR", "SNR")
-plots(train_data_blip[0], train_data_injections[0], chisq, chisq, "Chisq", "Chisq")
-plots(train_data_blip[0], train_data_injections[0], mass_1, mass_1, "Mass 1", "Mass 1")
-plots(train_data_blip[0], train_data_injections[0], mass_2, mass_2, "Mass 2", "Mass 2")
-plots(train_data_blip[0], train_data_injections[0], spin1z, spin1z, "Spin 1z", "Spin 1z")
-plots(train_data_blip[0], train_data_injections[0], spin2z, spin2z, "Spin 2z", "Spin 2z")
-
-plots(train_data_blip[0], train_data_injections[0], snr, chisq, "SNR", "Chisq")
-plots(train_data_blip[0], train_data_injections[0], snr, mass_1, "SNR", "Mass 1")
-plots(train_data_blip[0], train_data_injections[0], snr, mass_2, "SNR", "Mass 2")
-plots(train_data_blip[0], train_data_injections[0], snr, spin1z, "SNR", "Spin 1z")
-plots(train_data_blip[0], train_data_injections[0], snr, spin2z, "SNR", "Spin 2z")
-
-plots(train_data_blip[0], train_data_injections[0], chisq, mass_1, "Chisq", "Mass 1")
-plots(train_data_blip[0], train_data_injections[0], chisq, mass_2, "Chisq", "Mass 2")
-plots(train_data_blip[0], train_data_injections[0], chisq, spin1z, "Chisq", "Spin 1z")
-plots(train_data_blip[0], train_data_injections[0], chisq, spin2z, "Chisq", "Spin 2z")
-
-plots(train_data_blip[0], train_data_injections[0], mass_1, mass_2, "Mass 1", "Mass 2")
-plots(train_data_blip[0], train_data_injections[0], mass_1, spin1z, "Mass 1", "Spin 1z")
-plots(train_data_blip[0], train_data_injections[0], mass_1, spin2z, "Mass 1", "Spin 2z")
-
-plots(train_data_blip[0], train_data_injections[0], mass_2, spin1z, "Mass 2", "Spin 1z")
-plots(train_data_blip[0], train_data_injections[0], mass_2, spin2z, "Mass 2", "Spin 2z")
-
-plots(train_data_blip[0], train_data_injections[0], spin1z, spin2z, "Spin 1z", "Spin 2z")
--- code/plots_ana.py
+++
@@ -1,7 +0,0 @@
-def plots(blip, param1, param2, param1_name, param2_name):
-    plt.scatter(blip.param1, train_data_blip[0].param2, label = "Blip", s = 1)
-    plt.scatter(injections.param1, train_data_injections[0].param2, label = "Injections", s = 1)
-    plt.legend()
-    plt.title(param1_name + " vs " + param2_name)
-    plt.savefig("plots/" + param1_name + "vs" + param2_name + ".png")
-    plt.clf()
