f8d1abe72cf787e8843d47027761ff72a3195800
Anna Silva Martins <amartins@stbc-i1.nikhef.nl>
2023-01-01 17:28:39 +0100

    variable net results

--- src/VariableNet.py
+++ src/VariableNet.py
@@ -1,26 +1,30 @@
 from torch import nn
+import torch.nn.functional as F
 
 class VariableNet(nn.Module):
     def __init__(self, n_units, n_layers, a):
         super().__init__()
 
         if a == "ReLU":
-            self.activation = nn.ReLU()
+            self.activation = F.relu
 
         self.n_layers = n_layers
 
-        self.layers = nn.ModuleDict()
-        self.layers["input"] = nn.Linear(6, n_units)
+        self.layer = nn.ModuleList()
 
-        for i in range(self.n_layers):
-            self.layers[str(i)] = nn.Linear(n_units, n_units)
+        self.layer.append(nn.Linear(6, n_units))
 
-        self.layers["output"] = nn.Linear(n_units, 7)
+        for i in range(1, self.n_layers + 1):
+            self.layer.append(nn.Linear(n_units, n_units))
+
+        self.layer.append(nn.Linear(n_units, 7))
 
     def forward(self, x):
-        x = self.layers["input"](x)
+        x = self.layer[0](x)
 
-        for i in range(self.n_layers):
-            x = self.activation(self.layers[str(i)](x))
+        for i in range(1, self.n_layers + 1):
+            x = self.activation(self.layer[i](x))
 
-        return self.layers["output"](x)
+        x = self.layer[self.n_layers + 1](x)
+
+        return x
